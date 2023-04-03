ef7f1910e811529f5d246905ceff512149104ee8
Brian Parker <brian.parker@hotmail.com.au>
2023-04-04 03:32:15 +1000

    Add project files.

---
+++ Standard.AI.OpenAI.WASM/Client/Program.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Standard.AI.OpenAI.WASM.Client;
+
+namespace Standard.AI.OpenAI.WASM.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
